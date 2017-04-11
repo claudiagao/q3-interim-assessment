e82ec1ac9e1764d6a01ad4019d40e3384db4a036
cgao <cgao@wlapcs.org>
2017-04-11 18:42:40 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var numbers = [15, 20, 39, 10];
+var words = ["I tried", "Hurray", "Test", "Okay"];
+
+
+function setup(){
+    createCanvas(600, 400);
+}
+
+function draw(){
+    background(42, 220, 237);
+
+    ellipse(x, y, 100, 100);
+    fill(244, 66, 146);
+    textSize(30);
+
+}
